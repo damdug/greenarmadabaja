f6b7ad42eb4626b427bfa86178293edcd9e5e22d
damdug <155848353+damdug@users.noreply.github.com>
2024-05-29 17:36:21 -0700

    vercel running

--- api/saveUserInfo.js
+++ api/saveUserInfo.js
@@ -1,15 +1,18 @@
-const { createClient } = require('@supabase/supabase-js');
+// api/saveUserInfo.js
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const { name, email } = req.body;
 
-  const { data, error } = await supabase.from('users').insert([{ name, email }]);
+  const { data, error } = await supabase
+    .from('users')
+    .insert([{ name, email }]);
 
   if (error) {
-    res.status(500).json({ error: error.message });
-  } else {
-    res.status(200).json(data[0]);
+    return res.status(500).json({ error: error.message });
   }
+
+  return res.status(200).json({ data });
 };
